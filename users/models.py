1970c435241324f3659c09eed4d0a064e283c934
Fatih <16fay61@gmail.com>
2023-08-07 11:32:46 +0200

    add

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+#! Django nun default olan User modelini kullandik bu projede, o yuzden burada birsey yok
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.generics import (CreateAPIView)
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.authtoken.models import Token
+# from rest_framework.decorators import api_view
+class RegisterView(CreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=RegisterSerializer
