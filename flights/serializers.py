8bf6ea58d890440941e92b533900b5d2abb3a2d1
Fatih <16fay61@gmail.com>
2023-08-21 09:16:29 +0200

    add

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,25 +1,33 @@
 from rest_framework import serializers
 from .models import *
 
-class FlightSerializer(serializers.ModelSerializer):
-
+class FlightSerializer(serializers.ModelSerializer):   
     class Meta:
         model=Flight
-        fields=("__all__")
+        fields="__all__"
 
 class PassengerSerializer(serializers.ModelSerializer):
-
     class Meta:
         model=Passenger
-        fields=("__all__")
+        fields="__all__"
+
 
 class ReservationSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()  # Kullanıcı adını string olarak göstermek için
-    flight = FlightSerializer()  # Flight modeli için bir serializer kullanılıyor
-    passenger = PassengerSerializer(many=True)  # Birden çok yolcu için PassengerSerializer kullanılıyor
+    flight=serializers.StringRelatedField()
+    flight_id=serializers.IntegerField()
 
+    user=serializers.StringRelatedField()
+    user_id=serializers.IntegerField()
+    # passenger=serializers.StringRelatedField()
+    # passenger=serializers.IntegerField()
+    passenger= PassengerSerializer(many=True)
     class Meta:
-        model = Reservation
-        fields = "__all__"
-
-
+        model=Reservation
+        fields=(
+            "id",
+            "flight", 
+            "flight_id", 
+            "user",  
+            "user_id",
+            "passenger"
+            )
